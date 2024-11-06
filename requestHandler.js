f7f412bb8e09c5a76aa143bbac9daa0964227022
Sindhu5653 <sindhupvly@gmail.com>
2024-11-07 01:00:24 +0530

    wednesday night commit 1

--- requestHandler.js
+++ requestHandler.js
@@ -28,7 +28,7 @@ export async function adduser(req,res) {
         return res.status(500).send({msg:"password missmatch"})
 
     const check=await userSchema.find({email})
-    if(check)
+    if(check.length>0)
         return res.status(500).send({msg:"email already exist"})
 
 
@@ -162,7 +162,24 @@ export async function generateOtp(req,res) {
         to: email,
         subject: "OTP ",
         text: "Verify",
-        html: `<b>Hacked! otp is ${otp}</b>`,
+        html: `
+            <div style="font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;line-height:2">
+            <div style="margin:50px auto;width:80%;padding:20px 0">
+            <div style="border-bottom:5px solid #eee">
+              <a href="" style="font-size:30px;color: #f7c800;text-decoration:none;font-weight:600">FSA Technologies</a>
+            </div>
+            <p style="font-size:15px">Hello,</p>
+            <p>Thank you for choosing this site. Use this OTP to complete your Sign Up procedures and verify your account</p>
+            <p>Remember, Never share this OTP with anyone.</p>
+            <h2 style="background: #00466a;margin: 0 auto;width: max-content;padding: 0 10px;color: #fff;border-radius: 4px;">${otp}</h2>
+            <p style="font-size:15px;">Best regards,<br />#</p>
+            <hr style="border:none;border-top:5px solid #eee" />
+            <div style="float:right;padding:8px 0;color:#aaa;font-size:0.8em;line-height:1;font-weight:300">
+                <p>App Name Inc</p>
+                <p>address</p>
+            </div>
+            </div>
+            </div>`,
         
       })
       console.log("Message sent: %s", info.messageId)
